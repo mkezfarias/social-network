54f465e58f50ce1cef0d03865650bb7e73a79c87
Luis Puente <puente.luism@gmail.com>
2020-02-04 16:59:31 -0600

    Add test for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Like, type: :model do
     end
   end
 
+  describe 'like count' do
+    it 'increments the count of likes in a post by 1' do
+      user = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user, email: "aaa@aaaaa.com")
+      post = user.posts.create(content: 'hello hellow')
+      expect(post.likes.count).to eql(0)
+      post.likes.create(user: user)
+      expect(post.likes.count).to eql(1)
+      post.likes.create(user: user2)
+      expect(post.likes.count).to eql(2)
+    end
+  end
+
 end
