6b8af721fd8d769c2ab0236e2d29dc4a805f5302
Luis Puente <puente.luism@gmail.com>
2020-02-07 15:13:58 -0600

    Add only friends post show functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def feed
-    @posts = Post.all.order(created_at: :desc)
+     @friendships = current_user.friendships.select{|friendship| friendship.friend.id if friendship.confirmed} +
+                    current_user.inverse_friendships.select{ |friendship| friendship.user.id if friendship.confirmed}
+    @posts = Post.where("user_id IN (:ids)", ids: @friendships + [current_user.id]).order(created_at: :desc)
     @post = Post.new
   end
 
