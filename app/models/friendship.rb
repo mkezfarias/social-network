66bed60a90477e7321cddca033652a4519aebeca
Luis Puente <puente.luism@gmail.com>
2020-02-13 16:26:09 -0600

    Add unit tests for models

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,7 @@
 class Friendship < ApplicationRecord
+
+  validates :friend_id, :user_id, presence: true
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-  validates :user_id, uniqueness: {scope: :friend_id}
+  validates_uniqueness_of :user_id, scope: :friend_id
 end
